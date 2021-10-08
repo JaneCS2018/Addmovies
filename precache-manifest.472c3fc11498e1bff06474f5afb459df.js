cc0fb663155c3979d73adae92c975c3d23c53497
JaneCS2018 <xiangliu2011@gmail.com>
2021-10-07 17:29:08 -0700

    Updates

---
+++ precache-manifest.472c3fc11498e1bff06474f5afb459df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc2c48baf067aedb15700766d51ce31",
+    "url": "/Addmovies/index.html"
+  },
+  {
+    "revision": "fd950053463f9f4c4cca",
+    "url": "/Addmovies/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/Addmovies/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Addmovies/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd950053463f9f4c4cca",
+    "url": "/Addmovies/static/js/main.e331aa01.chunk.js"
+  },
+  {
+    "revision": "086f0c19f821c7b0d671",
+    "url": "/Addmovies/static/js/runtime-main.72e4109c.js"
+  }
+]);
